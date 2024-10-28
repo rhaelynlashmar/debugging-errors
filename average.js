e680078b0a8fe0ccade7a82930d0545005023064
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-10-27 20:55:21 -0700

    Debugged and commented why as to why

--- average.js
+++ average.js
@@ -1,10 +1,12 @@
-function average(list) {
-    var sum = 0;
-  
-    for (var num of list) {
-      sum ++ num;
-    }
-  
-    return sum / list.length;
-  
+const average = function(list) {
+  // the functions name is average, so we're looking for the average number of the array
+  let sum = 0;
+// I changed all the syntax to the most modernly used 
+  for (let num of list) {
+// ++ adds one to the variable- being equal to sum + 1 whereas sum = num + sum 
+    sum += num;
+  }
+
+  return sum / list.length;
+}
 console.log(average([3, 5, 7]));
