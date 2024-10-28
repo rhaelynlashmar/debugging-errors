e664ccacf1ab6706ad4110860dc269395db1b828
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-10-27 21:23:42 -0700

    Debugged and commented on errors

--- reverse.js
+++ reverse.js
@@ -1,8 +1,12 @@
-var input = process.argv[2];
-
-if (input)
+let input = process.argv[2];
+// changed the order so reverse could be initialized
+// original was spelt wrong and didn't match the parameter
+// reverseList isn't a method
+const reverse = function(original) {
+  return original.split('').reverse().join('');
+};
+// added in squiggly brackets to ensure it behaves as a loop
+if (input) {
   console.log(reverse(input));
-
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
+};
+// tested it with the word input to get tupni as an answer
